3aac802f33b9091a5a6fdcac273770744a8eac42
SorerBOT <alonfiller@gmaill.com>
2025-06-07 17:22:20 +0300

    (Feat) now added scheduler

--- CPU-Scheduler.c
+++ CPU-Scheduler.c
@@ -1,9 +1,12 @@
+#include <signal.h>
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <time.h>
 #include <unistd.h>
+
+#define TICK_TIME 1e5
 
 #define MAX_NAME 51
 #define MAX_DESC 101
@@ -31,14 +34,22 @@ typedef struct
     int (*CmpPriority)(Process, Process);
 } ReadyQueue;
 
+
+
 int CmpPriorityNull(Process _, Process __);
 int ProcCmpPriority(Process a, Process b, int (*getProcessPriority)(Process));
 void Enqueue(ReadyQueue* queue, Process item);
 Process Dequeue(ReadyQueue* queue);
+bool IsEmpty(ReadyQueue queue);
 void InitProcessesFromCSV(const char* path, Process oprocs[], int* oprocsCount);
 Process ParseProcess(const char* line);
 void SortProcesses(Process procs[], int procCount, int (*predicate)(Process, Process));
 int ProcCmpArrivalTime(Process a, Process b);
+double GetTimeElapsed(struct timespec startingTime);
+void EnqueueNewArrivals(ReadyQueue* queue, Process procs[], int* startingIdx, int procCount, struct timespec startingTime);
+void SigAlarmHandler();
+
+
 
 void HandleCPUScheduler(const char* processesCsvFilePath, int timeQuantum)
 {
@@ -63,25 +74,108 @@ void HandleCPUScheduler(const char* processesCsvFilePath, int timeQuantum)
     /*
      * Initialise Ready Queue (FCFS)
      */
-    ReadyQueue queue =
-    {
-        .CmpPriority = CmpPriorityNull,
-        .size = 0,
-    };
+    ReadyQueue queue;
+    queue.size = 0;
+    queue.CmpPriority = CmpPriorityNull;
+
+
+
+    /*
+     * Ignoring sigalarms, we use them as a waiting mechanism
+     */
+    struct sigaction act;
+    act.sa_handler = SigAlarmHandler;
+    act.sa_flags = 0;
+    sigemptyset(&act.sa_mask);
+    sigaction(SIGALRM, &act, NULL);
 
 
 
     /*
      * Start timer
      */
-    struct timespec startingTime, currentTime;
-    clock_gettime(CLOCK_MONOTONIC, &startingTime);
+    struct timespec startingTime;
+    if (clock_gettime(CLOCK_MONOTONIC, &startingTime) != 0)
+    {
+        perror("clock_gettime() error");
+        exit(EXIT_FAILURE);
+    }
 
-    sleep(5);
 
-    clock_gettime(CLOCK_MONOTONIC, &currentTime);
-    double elapsed = (currentTime.tv_sec - startingTime.tv_sec) +
-        (currentTime.tv_nsec - startingTime.tv_nsec) / 1e9;
+
+    /*
+     * Main loop which runs every tick (1 second) and:
+     * Enqueues new processes
+     * Checks if the process currently running has finished
+     *
+     */
+    int startingIdx = 0;
+    Process runningProcess;
+    bool isProcessRunning = false;
+    bool isIdling = false;
+    struct timespec processStartingTime;
+    bool isProcessNotArrived = startingIdx < procsCount;
+    int turnaroundTime = 0;
+
+    while (isProcessNotArrived || IsEmpty(queue) || isProcessRunning)
+    {
+        isProcessNotArrived = startingIdx < procsCount;
+
+
+
+        if (isProcessNotArrived)
+            EnqueueNewArrivals(&queue, procs, &startingIdx, procsCount, startingTime);
+
+
+
+        if (isProcessRunning)
+        {
+            int timeElapsed = GetTimeElapsed(processStartingTime);
+            if (timeElapsed >= runningProcess.burst_time)
+            {
+                isProcessRunning = false;
+                if (!isProcessNotArrived && IsEmpty(queue))
+                {
+                    turnaroundTime = GetTimeElapsed(startingTime);
+                    break;
+                }
+            }
+        }
+
+
+
+        if (!isProcessRunning && !IsEmpty(queue))
+        {
+            if (isIdling)
+            {
+                isIdling = false;
+            }
+            isProcessRunning = true;
+            if (clock_gettime(CLOCK_MONOTONIC, &processStartingTime) != 0)
+            {
+                perror("clock_gettime() error");
+                exit(EXIT_FAILURE);
+            }
+            runningProcess = Dequeue(&queue);
+        }
+
+
+
+
+        if (!isProcessRunning && IsEmpty(queue) && isProcessNotArrived)
+        {
+            isIdling = true;
+        }
+
+
+
+        ualarm((int)1e5, 0);
+        pause();
+    }
+
+
+
+    printf("Turnaround time: %d\n", turnaroundTime);
 }
 
 
@@ -197,7 +291,6 @@ int CmpPriorityNull(Process _, Process __)
     return 0;
 }
 
-
 int ProcCmpArrivalTime(Process a, Process b)
 {
     return a.arrival_time - b.arrival_time;
@@ -225,6 +318,12 @@ void SortProcesses(Process procs[], int procCount, int (*predicate)(Process, Pro
 
 Process Dequeue(ReadyQueue* queue)
 {
+    if (IsEmpty(*queue))
+    {
+        fprintf(stderr, "Invalid operation error: queue is already empty");
+        exit(EXIT_FAILURE);
+    }
+
     Process firstProcess = queue->procs[0];
 
     queue->size--;
@@ -236,7 +335,65 @@ Process Dequeue(ReadyQueue* queue)
 
 void Enqueue(ReadyQueue* queue, Process item)
 {
+    if (queue->size >= MAX_PROC)
+    {
+        fprintf(stderr, "Invalid operation error: queue is full");
+        exit(EXIT_FAILURE);
+    }
     queue->procs[queue->size] = item;
     queue->size++;
     SortProcesses(queue->procs, queue->size, queue->CmpPriority);
 }
+
+double GetTimeElapsed(struct timespec startingTime)
+{
+    struct timespec currentTime;
+
+    if (clock_gettime(CLOCK_MONOTONIC, &currentTime) != 0)
+    {
+        perror("clock_gettime error");
+        exit(EXIT_FAILURE);
+    }
+
+    return  (currentTime.tv_sec - startingTime.tv_sec) +
+            (currentTime.tv_nsec - startingTime.tv_nsec) / 1e9;
+}
+
+void EnqueueNewArrivals(ReadyQueue* queue, Process procs[], int* startingIdx, int procCount, struct timespec startingTime)
+{
+    /*
+     * Validating that there is a process to add
+     */
+    if (*startingIdx >= procCount)
+    {
+        fprintf(stderr, "Invalid argument error: startingIdx = %d when there are only %d processes\n", *startingIdx, procCount);
+        exit(EXIT_FAILURE);
+    }
+
+
+
+    /*
+     * Getting scheduler uptime
+     */
+    double uptime = GetTimeElapsed(startingTime);
+
+
+
+    /*
+     * Adding processes to ReadyQueue
+     */
+    for (int i = *startingIdx; i < procCount; i++)
+        if (procs[i].arrival_time < uptime)
+        {
+            Enqueue(queue, procs[i]);
+            (*startingIdx)++;
+        }
+        else break;
+}
+
+bool IsEmpty(ReadyQueue queue)
+{
+    return queue.size == 0;
+}
+
+void SigAlarmHandler() {  }
